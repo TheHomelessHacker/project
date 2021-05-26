f8bf1356cdb2293f20d0747c3a175ec3cba051e6
Mark <markzdorov13@icloud.com>
2021-05-26 19:11:26 +0300

    first commit

---
+++ app/src/main/java/com/example/newprojectoldtown/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.newprojectoldtown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn,btn2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButton ();
+    }
+    public void addListenerOnButton(){
+        btn = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent(".SecondActivity");
+            startActivity(intent);
+        });
+        btn2.setOnClickListener(view -> {
+            Intent intent = new Intent(".ThirdActivity");
+            startActivity(intent);
+        });
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/newprojectoldtown/ThirdActivity.java
@@ -0,0 +1,14 @@
+package com.example.newprojectoldtown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+// 2 кнопка
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
